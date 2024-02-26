ca43f5e5e77eca289ee5995ac2755739cc584410
Nikolai-Gith <mnickslm7@gmail.com>
2024-02-26 19:00:34 +0200

    basic react and server connect

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require('express-async-errors');
 const express = require('express');
 const logger = require('morgan');
 const { errorHandler } = require('./middlewares/errorHandler');
+const cors = require('cors'); // Import cors middleware
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,6 +21,30 @@ app.use(errorHandler);
 
 app.use((req, res) => {
   res.status(400).send("Couldn't connect");
+});
+
+// Set up CORS middleware
+const allowedOrigins = ['http://localhost:5173']; // Update with your frontend URL
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
+app.post('/api/login', (req, res) => {
+  const { login, password } = req.body;
+  console.log('Received login:', login);
+  console.log('Received password:', password);
+
+  // No need to manually set CORS headers here
+
+  // Respond to the request
+  res.json({ message: 'Login received successfully' });
 });
 
 app.listen(port, () => console.log(`Express server is running on port ${port}`));
