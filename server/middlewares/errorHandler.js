6e11b25c829dc40a167862c29517cdd12c9e810d
Adi Levi <adile0194@gmail.com>
2024-03-29 20:20:37 +0300

    update error handler exports

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,6 +1,4 @@
-const errorHandler = (error, req, res, next) => {
+exports.errorHandler = (error, req, res, next) => {
   res.status(error.status || 500);
   return res.json({ message: error.message || "Internal Server Error" });
 };
-
-module.exports = errorHandler;
