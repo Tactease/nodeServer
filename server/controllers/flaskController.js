02c3fd4caa2f1656340801b2a706acbe2278cdef
Adi Levi <adile0194@gmail.com>
2024-05-15 17:24:12 +0300

    updated payload

--- server/controllers/flaskController.js
+++ server/controllers/flaskController.js
@@ -6,19 +6,30 @@ const axios = require('axios');
 localURL = 'http://localhost:5000/';
 rendeURL = 'https://tacteasepythonserver.onrender.com/';
 
-
 exports.flaskController = {
   async flaskConnection(requestString, data) {
-    const flaskApiUrl = `https://tacteasepythonserver.onrender.com/${requestString}`;
-    let payload = {
-      missions: JSON.stringify(data.missions),
-      soldiers: JSON.stringify(data.soldiers)
-    };
+    const flaskApiUrl = `${localURL}${requestString}`;
+    let payload = {};
+
+    if (requestString === 'generae_schedule') {
+      payload = {
+        missions: JSON.stringify(data.missions),
+        soldiers: JSON.stringify(data.soldiers)
+      };
+    }
 
     if (requestString === 'add_mission') {
       payload = {
         schedule: JSON.stringify(data.schedule),
         new_mission: JSON.stringify(data.new_mission),
+        soldiers: JSON.stringify(data.soldiers)
+      };
+    }
+
+    if (requestString === 'change_soldier_upon_request_approved') {
+      payload = {
+        request_approved: JSON.stringify(data.request_approved),
+        missions: JSON.stringify(data.missions),
         soldiers: JSON.stringify(data.soldiers)
       };
     }
